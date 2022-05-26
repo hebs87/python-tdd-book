34b61f0331675015b6d0a303bb1c7b0020f7bcc6
sunny <sunny@jbcole.co.uk>
2022-05-26 17:25:17 +0100

    Updates home_page POST test and logic to allow for creating Item record and returning data to template

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
+from .models import Item
+
 
 # Create your views here.
 def home_page(request):
+    item = Item()
+    item.text = request.POST.get('item_text', '')
+    item.save()
+
     return render(
         request,
         'home.html',
-        {'new_item_text': request.POST.get('item_text', '')}
+        {'new_item_text': item.text}
     )
